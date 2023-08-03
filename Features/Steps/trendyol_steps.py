38498228ddd5f958960f2a054e1427f7480ae066
TugberkkCoskun <tugberk.coskun94@hotmail.com>
2023-08-03 17:56:43 +0300

    Trendyol BDD

--- Features/Steps/trendyol_steps.py
+++ Features/Steps/trendyol_steps.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException, InvalidSelectorException
 import time
+import random
 
 @given('User shall go to the www.trendyol.com and saw the main page is opened')
 def go_to_main_page(context):
@@ -39,9 +40,64 @@ def press_enter_button(context):
 def gomlek_results(context):
     try:
         result = context.driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[1]/div[1]/div/h1")
-        text = "gomlek"
+        text = "gömlek"
         if result.text != text:
-            raise
+            raise AssertionError
+    except NoSuchElementException:
+        pass
+
+@given('User shall go to the "https://www.trendyol.com/sr?q=g%C3%B6mlek&qt=g%C3%B6mlek&st=g%C3%B6mlek&os=1"')
+def go_to_gömlek_result_page(context):
+    context.driver = webdriver.Chrome()
+    context.wait = WebDriverWait(context.driver, 15)
+    context.driver.get("https://www.trendyol.com/sr?q=g%C3%B6mlek&qt=g%C3%B6mlek&st=g%C3%B6mlek&os=1")
+    context.driver.maximize_window()
+    context.wait.until(EC.presence_of_element_located((By.ID,"onetrust-accept-btn-handler"))).click()
+    time.sleep(3)
+
+@when('User shall randomly select one of the listed results')
+def randomly_select(context):
+    context.driver.execute_script("window.scrollBy(0, 100)")
+    try:
+        pop_up_element = context.driver.find_element(By.XPATH,"//div[@class='popup']")
+        overlay_element = context.driver.find_element(By.XPATH,"//div[@class='overlay']")
+        if pop_up_element.is_displayed() or overlay_element.is_displayed():
+            action = ActionChains(context.driver)
+            action.move_by_offset(100,100).perform()
+            action.click().perform()
+    except NoSuchElementException:
+        pass
+    gomleks = context.driver.find_elements(By.XPATH,"//div[@class='p-card-wrppr with-campaign-view']")
+    random_gomlek= random.choice(gomleks)
+    random_gomlek.click()
+@then('User shall add randomly selected gömlek to the basket')
+def add_to_basket(context):
+    window= context.driver.current_window_handle
+    new_window = context.driver.window_handles[-1]
+    context.driver.switch_to.window(new_window)
+    new_wind_url = context.driver.current_url
+    context.price_at_item_page = context.wait.until(EC.presence_of_element_located((By.XPATH,"//span[@class='prc-dsc']")))
+    context.price_at_item_list =[]
+    context.price_at_item_list.append(context.price_at_item_page.text)
+    basket_button = context.wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"[component-id='1']")))
+    basket_button.click()
+@then('User shall verify that the price of the item in the basket is the same with the one on the gömlek page')
+def verify_the_price(context):
+    my_basket = context.driver.find_element(By.XPATH,"//div[@class='account-nav-item basket-preview']")
+    action = ActionChains(context.driver)
+    action.move_to_element(my_basket).perform()
+    go_to_my_basket = context.wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "a.goBasket")))
+    go_to_my_basket.click()
+    price_at_my_basket = context.wait.until(EC.presence_of_element_located((By.XPATH,"//div[@class='pb-basket-item-price']")))
+    price_at_my_basket_list = []
+    price_at_my_basket_list.append(price_at_my_basket.text)
+
+    assert context.price_at_item_list == price_at_my_basket_list
+
+
+
+
+
 
 
 
--- Features/trendyol.feature
+++ Features/trendyol.feature
@@ -9,18 +9,17 @@ Feature: Trendyol Basic Automation Task
     When User enters "gömlek" in the text box
     When User shall presses the enter key on the keyboard
     Then User shall see the gömlek results are displayed
-
+  @gomlek_page
   Scenario: Trendyol Gömlek Page
     Given User shall go to the "https://www.trendyol.com/sr?q=g%C3%B6mlek&qt=g%C3%B6mlek&st=g%C3%B6mlek&os=1"
     When User shall randomly select one of the listed results
-    Then User shall see the randomly selected gömlek page is openen
-    And User shall add randomly selected gömlek to the basket
-    And User shall verify that the price of the item in the basket is the same with the one on the gömlek page
+    Then User shall add randomly selected gömlek to the basket
+    Then User shall verify that the price of the item in the basket is the same with the one on the gömlek page
 
   Scenario: Quantity increase or decrease
     Given User is on the selected gömlek page
     When User increase the quantity
-    Then User shall see the quantity is increased,
+    Then User shall see the quantity is increased
     And User shall verified that the quantity of the product is 2
     When User press the bin button
     Then User shall verify that the basket is empty
